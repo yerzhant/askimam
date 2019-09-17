feff758cba7692ff1ce3efa387de56d039765c36
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2019-09-17 14:50:45 +0600

    Setup iOS

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/yerzhan/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/yerzhan/projects/askimam"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/yerzhantulepov/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yerzhantulepov/dev/askimam"
+export "FLUTTER_TARGET=/Users/yerzhantulepov/dev/askimam/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/yerzhan/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yerzhantulepov/dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.12.1"
 export "FLUTTER_BUILD_NUMBER=19"
+export "TRACK_WIDGET_CREATION=true"
