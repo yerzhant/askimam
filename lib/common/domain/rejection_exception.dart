e445319f7c6a04f91d7fa4233ae8cd6013b5a854
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-17 16:39:42 +0600

    Remove unused class

--- lib/common/domain/rejection_exception.dart
+++
@@ -1,9 +0,0 @@
-import 'package:askimam/common/domain/rejection.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'rejection_exception.freezed.dart';
-
-@freezed
-abstract class RejectionException with _$RejectionException {
-  factory RejectionException(Rejection rejection) = _RejectionException;
-}
