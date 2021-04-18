9aefc1d825ce1e45d708081b619bfadc79fda16c
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-18 23:25:02 +0600

    Add auth bloc

--- lib/auth/bloc/auth_state.dart
+++ lib/auth/bloc/auth_state.dart
@@ -2,9 +2,9 @@ part of 'auth_bloc.dart';
 
 @freezed
 class AuthState with _$AuthState {
-  const factory AuthState.unauthenticated() = _Unauthenticated;
   const factory AuthState.inProgress() = _InProgress;
   const factory AuthState.error(Rejection rejection) = _Error;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
   const factory AuthState.authenticated(Authentication authentication) =
       _Authenticated;
 }
