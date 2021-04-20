0e5c6079fbd0f3f9fb0ce71eb07bf056a7d5ed9f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-20 20:51:35 +0600

    Implement notification service

---
+++ lib/common/infra/fcm_service.dart
@@ -0,0 +1,19 @@
+import 'package:askimam/common/domain/model/rejection.dart';
+import 'package:askimam/common/domain/service/notification_service.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FcmService implements NotificationService {
+  @override
+  Future<Either<Rejection, String>> getFcmToken() async {
+    try {
+      final token = await FirebaseMessaging.instance.getToken();
+
+      if (token == null) return left(Rejection('No FCM token is available.'));
+
+      return right(token);
+    } on Exception catch (e) {
+      return left(e.asRejection());
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+void main() async {
+  await Firebase.initializeApp();
+  _requestMessagingPermissions();
+}
+
+void _requestMessagingPermissions() async {
+  final messaging = FirebaseMessaging.instance;
+
+  final settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+}
