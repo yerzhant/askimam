74a60e1765f8f826e9efe6b508670f119de95df0
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-04-24 15:53:20 +0600

    Add my chats widget

---
+++ lib/chats/ui/widget/my_chats_widget.dart
@@ -0,0 +1,48 @@
+import 'package:askimam/chat/domain/model/chat.dart';
+import 'package:askimam/chats/bloc/my_chats_bloc.dart';
+import 'package:askimam/common/ui/widget/in_progress_widget.dart';
+import 'package:askimam/common/ui/widget/rejection_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyChatsWidget extends StatelessWidget {
+  const MyChatsWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MyChatsBloc, MyChatsState>(
+      builder: (context, state) {
+        return state.when(
+          (items) => _list(items, context),
+          inProgress: (items) => InProgressWidget(child: _list(items, context)),
+          error: (rejection) => RejectionWidget(
+            rejection: rejection,
+            onRefresh: () =>
+                context.read<MyChatsBloc>().add(const MyChatsEvent.reload()),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _list(List<Chat> items, BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async =>
+          context.read<MyChatsBloc>().add(const MyChatsEvent.reload()),
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (_, i) {
+          final item = items[i];
+
+          return Dismissible(
+            key: Key('$item'),
+            onDismissed: (_) =>
+                context.read<MyChatsBloc>().add(MyChatsEvent.delete(item)),
+            child: ListTile(title: Text(item.subject)),
+          );
+        },
+        physics: const AlwaysScrollableScrollPhysics(),
+      ),
+    );
+  }
+}
--- test/chats/ui/widget/my_chats_widget_test.dart
+++ test/chats/ui/widget/my_chats_widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:askimam/chat/domain/model/chat.dart';
 import 'package:askimam/chats/bloc/my_chats_bloc.dart';
-import 'package:askimam/chats/ui/widget/public_chats_widget.dart';
+import 'package:askimam/chats/ui/widget/my_chats_widget.dart';
+import 'package:askimam/common/domain/model/rejection.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -26,16 +27,47 @@ void main() {
     app = MaterialApp(
       home: BlocProvider(
         create: (BuildContext context) => bloc,
-        child: const Material(child: PublicChatsWidget()),
+        child: const Material(child: MyChatsWidget()),
       ),
     );
   });
 
   testWidgets('should show a list', (tester) async {
+    await _fixture(bloc, tester, app);
+
+    expect(find.text('Chat 1'), findsOneWidget);
+    expect(find.text('Chat 2'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  // TODO: fix it
+  testWidgets('should load a next page', (tester) async {
+    await _fixture(bloc, tester, app, count: 12);
+    expect(find.text('Chat 12'), findsNothing);
+    // await tester.drag(find.text('Chat 5'), const Offset(0.0, -300.0));
+    await tester.fling(find.text('Chat 5'), const Offset(0.0, -3000.0), 1000);
+    await tester.pump(const Duration(seconds: 10));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Chat 12'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    verify(() => bloc.add(const MyChatsEvent.loadNextPage())).called(1);
+  }, skip: true);
+
+  // TODO: fix it
+  testWidgets('should delete a chat', (tester) async {
+    await _fixture(bloc, tester, app);
+    await tester.drag(find.text('Chat 2'), const Offset(500, 0));
+    await tester.pumpAndSettle();
+
+    verify(() => bloc.add(MyChatsEvent.delete(Chat(2, 'Chat 2')))).called(1);
+  }, skip: true);
+
+  testWidgets('should show a list and a progress circle', (tester) async {
     whenListen(
       bloc,
       Stream.value(const MyChatsState([])),
-      initialState: MyChatsState([
+      initialState: MyChatsState.inProgress([
         Chat(1, 'Chat 1'),
         Chat(2, 'Chat 2'),
       ]),
@@ -45,6 +77,61 @@ void main() {
 
     expect(find.text('Chat 1'), findsOneWidget);
     expect(find.text('Chat 2'), findsOneWidget);
-    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
+
+  testWidgets('should refresh on pulling down', (tester) async {
+    await _fixture(bloc, tester, app);
+    await tester.fling(find.text('Chat 1'), const Offset(0.0, 300.0), 1000.0);
+    await tester.pumpAndSettle();
+
+    verify(() => bloc.add(const MyChatsEvent.reload())).called(1);
+  });
+
+  // TODO: add routing to a chat on tapping on an item
+  // TODO: add favorites stuff
+
+  testWidgets('should show an error', (tester) async {
+    await _errorFixture(bloc, tester, app);
+
+    expect(find.text('reason'), findsOneWidget);
+  });
+
+  testWidgets('should invoke a refresh on rejection', (tester) async {
+    await _errorFixture(bloc, tester, app);
+    await tester.tap(find.text('ОБНОВИТЬ'));
+
+    verify(() => bloc.add(const MyChatsEvent.reload())).called(1);
+  });
+}
+
+Future _fixture(
+  MyChatsBloc bloc,
+  WidgetTester tester,
+  Widget app, {
+  int count = 2,
+}) async {
+  whenListen(
+    bloc,
+    Stream.value(const MyChatsState([])),
+    initialState: MyChatsState([
+      for (var i = 1; i <= count; i++) Chat(i, 'Chat $i'),
+    ]),
+  );
+
+  await tester.pumpWidget(app);
+}
+
+Future _errorFixture(
+  MyChatsBloc bloc,
+  WidgetTester tester,
+  Widget app,
+) async {
+  whenListen(
+    bloc,
+    Stream.value(const MyChatsState([])),
+    initialState: MyChatsState.error(Rejection('reason')),
+  );
+
+  await tester.pumpWidget(app);
 }
