dec84c4003e1a553aecb79dbdb58d957e1542ebf
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-03 22:46:07 +0600

    Change local address

--- lib/common/connection.dart
+++ lib/common/connection.dart
@@ -1,6 +1,7 @@
 const apiUrl = String.fromEnvironment(
   'api-url',
-  defaultValue: 'http://10.0.2.2:8080',
+  // defaultValue: 'http://10.0.2.2:8080',
+  defaultValue: 'http://192.168.0.102:8080',
 );
 
 const audioUrl = '$apiUrl/askimam/audio';
