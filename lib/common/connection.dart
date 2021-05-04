a8ca2612f69df185a64613b4bd08f1408be31e3f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-05 03:15:22 +0600

    Add server url

--- lib/common/connection.dart
+++ lib/common/connection.dart
@@ -1,7 +1,8 @@
-const apiUrl = String.fromEnvironment(
-  'api-url',
+const serverUrl = String.fromEnvironment(
+  'server-url',
   // defaultValue: 'http://10.0.2.2:8080',
   defaultValue: 'http://192.168.0.102:8080',
 );
+const apiUrl = '$serverUrl/v1';
 
-const audioUrl = '$apiUrl/askimam/audio';
+const audioUrl = '$serverUrl/askimam/audio';
