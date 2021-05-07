56beaa3d609301eeb389c5a68d050c4638333852
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-07 21:13:42 +0600

    Add web support

---
+++ build-android.sh
@@ -0,0 +1,2 @@
+flutter build appbundle --dart-define=api-url=https://api.askimam.azan.kz/v1 --dart-define=audio-url=https://azan.kz/askimam/audio
+# flutter build apk --split-per-abi --dart-define=api-url=https://api.askimam.azan.kz/v1 --dart-define=audio-url=https://azan.kz/askimam/audio
