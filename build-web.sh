7475931b5ad1c484d9d844b16c2e2f3fd134ad7a
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-08 16:32:34 +0600

    Push web image

--- build-web.sh
+++ build-web.sh
@@ -1,3 +1,4 @@
 version=0.0.2
 flutter build web --dart-define=api-url=https://api.askimam.azan.kz/v1 --dart-define=audio-url=https://azan.kz/askimam/audio
 docker build -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version .
+docker push $AskimamRegistry/askimam:$version
