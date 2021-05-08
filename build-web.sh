4510e0f465f386e9a5d7ea9dbed1fcfa2a9859b9
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-08 17:39:52 +0600

    Add dev image

--- build-web.sh
+++ build-web.sh
@@ -1,4 +1,9 @@
-version=0.0.2
+version=0.0.3
+
 flutter build web --dart-define=api-url=https://api.askimam.azan.kz/v1 --dart-define=audio-url=https://azan.kz/askimam/audio
 docker build -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version .
 docker push $AskimamRegistry/askimam:$version
+
+flutter build web --dart-define=api-url=http://api.askimam.azan-dev/v1 --dart-define=audio-url=http://azan-dev/askimam/audio
+docker build -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version-dev .
+docker push $AskimamRegistry/askimam:$version-dev
