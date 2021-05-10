7a86782284d3b7be55f65f0dc476474d147ff3c2
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-05-10 19:44:52 +0600

    Add imam ratings page

---
+++ lib/imam_ratings/ui/imam_ratings_page.dart
@@ -0,0 +1,57 @@
+import 'package:askimam/common/ui/ui_constants.dart';
+import 'package:askimam/common/ui/widget/in_progress_widget.dart';
+import 'package:askimam/common/ui/widget/rejection_widget.dart';
+import 'package:askimam/imam_ratings/bloc/imam_ratings_bloc.dart';
+import 'package:askimam/imam_ratings/domain/model/imam_rating.dart';
+import 'package:auto_direction/auto_direction.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ImamRatingsPage extends StatelessWidget {
+  final ImamRatingsBloc bloc;
+
+  ImamRatingsPage({Key? key, required this.bloc}) : super(key: key) {
+    bloc.add(const ImamRatingsEvent.reload());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Рейтинг устазов')),
+      body: BlocBuilder<ImamRatingsBloc, ImamRatingsState>(
+        bloc: bloc,
+        builder: (context, state) {
+          return state.when(
+            (ratings) => _list(ratings),
+            inProgress: () => InProgressWidget(child: Container()),
+            error: (rejection) => RejectionWidget(
+              rejection: rejection,
+              onRefresh: () {},
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _list(List<ImamRating> items) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (_, i) {
+        final item = items[i];
+
+        return ListTile(
+          title: AutoDirection(
+            text: item.name,
+            child: Text(item.name),
+          ),
+          trailing: Text(item.rating.toString()),
+        );
+      },
+      physics: const BouncingScrollPhysics(
+        parent: AlwaysScrollableScrollPhysics(),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: basePadding),
+    );
+  }
+}
---
+++ test/imam_ratings/ui/imam_ratings_page_test.dart
@@ -0,0 +1,50 @@
+import 'package:askimam/common/ui/widget/circular_progress.dart';
+import 'package:askimam/imam_ratings/bloc/imam_ratings_bloc.dart';
+import 'package:askimam/imam_ratings/domain/model/imam_rating.dart';
+import 'package:askimam/imam_ratings/ui/imam_ratings_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'imam_ratings_page_test.mocks.dart';
+
+@GenerateMocks([ImamRatingsBloc])
+void main() {
+  late Widget app;
+  late ImamRatingsBloc bloc;
+
+  setUp(() {
+    bloc = MockImamRatingsBloc();
+    when(bloc.state).thenReturn(const ImamRatingsState.inProgress());
+    when(bloc.stream).thenAnswer((_) => const Stream.empty());
+
+    app = MaterialApp(
+      home: ImamRatingsPage(bloc: bloc),
+    );
+  });
+
+  testWidgets('should be in progress', (tester) async {
+    await tester.pumpWidget(app);
+
+    expect(find.byType(CircularProgress), findsOneWidget);
+
+    verify(bloc.add(const ImamRatingsEvent.reload())).called(1);
+  });
+
+  testWidgets('should show ratings', (tester) async {
+    when(bloc.state).thenReturn(ImamRatingsState([
+      ImamRating('Imam 1', 123),
+      ImamRating('Imam 2', 12),
+    ]));
+
+    await tester.pumpWidget(app);
+
+    expect(find.text('Рейтинг устазов'), findsOneWidget);
+    expect(find.text('Imam 1'), findsOneWidget);
+    expect(find.text('Imam 2'), findsOneWidget);
+    expect(find.text('123'), findsOneWidget);
+    expect(find.text('12'), findsOneWidget);
+  });
+}
