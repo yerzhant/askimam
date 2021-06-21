ab96d6f36b95d56618a1aad563ab1b1e6e80dbb0
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-06-21 13:23:53 +0600

    Use docker's buildx

--- build-web.sh
+++ build-web.sh
@@ -2,8 +2,9 @@ version=1.5.0
 
 flutter test || exit
 flutter build web --dart-define=api-url=https://api.askimam.azan.kz/v1 --dart-define=audio-url=https://azan.kz/askimam/audio
-docker build -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version .
-docker push $AskimamRegistry/askimam:$version
+docker buildx build --platform linux/amd64,linux/arm64 -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version --push .
+# docker build -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version .
+# docker push $AskimamRegistry/askimam:$version
 
 # flutter build web --dart-define=api-url=http://api.askimam.azan-dev/v1 --dart-define=audio-url=http://azan-dev/askimam/audio
 # docker build -f nginx/Dockerfile -t $AskimamRegistry/askimam:$version-dev .
