86285c79b5a911b84ae92beb1a20126fb91c44ad
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2024-01-27 00:44:39 +0600

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:askimam/app_module.dart';
 import 'package:askimam/app_widget.dart';
 import 'package:askimam/common/connection.dart';
+import 'package:askimam/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/widgets.dart';
@@ -9,7 +10,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseMessaging.instance.requestPermission();
 
   runApp(ModularApp(module: AppModule(apiUrl), child: AppWidget()));
