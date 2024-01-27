e8977610c1be617084e99fb45bba0a11835a8b65
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2024-01-27 21:37:41 +0600

    Comment out old props in theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Вопрос имаму',
       theme: ThemeData(
-        accentColor: primaryColor,
+        // TODO
+        // accentColor: primaryColor,
         colorScheme: const ColorScheme(
           primary: primaryColor,
-          primaryVariant: primaryDarkColor,
+          // TODO
+          // primaryVariant: primaryDarkColor,
           secondary: secondaryColor,
-          secondaryVariant: secondaryDarkColor,
+          // TODO
+          // secondaryVariant: secondaryDarkColor,
           surface: Colors.green,
           background: Colors.yellow,
           error: Colors.green,
