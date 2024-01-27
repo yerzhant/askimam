b46a4c5a79d2477de2b9a362b6b2c01e226a44b6
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2024-01-27 17:24:29 +0600

    RW search bloc

--- lib/home/search/bloc/search_chats_event.dart
+++ lib/home/search/bloc/search_chats_event.dart
@@ -1,6 +1,17 @@
 part of 'search_chats_bloc.dart';
 
-@freezed
-class SearchChatsEvent with _$SearchChatsEvent {
-  const factory SearchChatsEvent.find(String phrase) = _Find;
+sealed class SearchChatsEvent extends Equatable {
+  const SearchChatsEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class SearchChatsEventFind extends SearchChatsEvent {
+  final String phrase;
+
+  const SearchChatsEventFind(this.phrase);
+
+  @override
+  List<Object?> get props => [phrase];
 }
